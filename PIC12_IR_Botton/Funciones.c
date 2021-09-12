504b431c52f7cb317665aa39270ede5c7db4083a
Antonio Pateti <50499248+apateti@users.noreply.github.com>
2021-09-12 07:48:12 -0400

    Add files via upload

---
+++ PIC12_IR_Botton/Funciones.c
@@ -0,0 +1,465 @@
+/* 
+ * File:   Funciones.c
+ * Author: APateti
+ *
+ * Created on 16 de julio de 2015, 03:30 PM
+ */
+/*
+ *Se Definen las Funciones a Utilizar en el Programa Principal
+*/
+#include <xc.h>
+#define	baud		12		//Baudios a 19200
+#define	Desh_INT_Rx 	PIE1bits.RCIE = 0
+#define	Hab_INT_Rx 		PIE1bits.RCIE = 1
+
+//****************************************************************
+//	Funcion que Configura el TIMER2 para scan de ver si se Rx IR
+//	Entrada: Config_T2
+//  T2 = (4/4MHz) * PreDiv * PosDiv * (PR2+1)
+//  T2 = 16 mSeg
+//	Con Post en 10 da 160 mseg
+//****************************************************************
+//
+void Config_T2(void)
+{
+	PR2 = 249;
+	T2CONbits.T2CKPS = 0b11;		//PreDiv 64
+	T2CONbits.T2OUTPS = 0b0000;		//PosDiv 1
+	//T2CONbits.T2OUTPS = 0b1001;		//PosDiv 10
+	T2CONbits.TMR2ON = 0;
+	TMR2 = 0;
+}
+//****************************************************************
+//	Funcion que Configura el TIMER2 para scan de ver si se Rx IR
+//	Entrada: Config_T2_FinIR
+//  T2 = (4/4MHz) * PreDiv * PosDiv * (PR2+1)
+//  T2 = 48 mSeg
+//	Con Post en 10 da 160 mseg
+//****************************************************************
+//
+void Config_T2_FinIR(void)
+{
+	PR2 = 249;
+	T2CONbits.T2CKPS = 0b11;		//PreDiv 64
+	T2CONbits.T2OUTPS = 0b00100;		//PosDiv 3
+	//T2CONbits.T2OUTPS = 0b1001;		//PosDiv 10
+	T2CONbits.TMR2ON = 0;
+	TMR2 = 0;
+}
+//****************************************************************
+//	Funcion que Configura el TIMER0 para scan de mTouch
+//	Entrada: Config_T0
+//****************************************************************
+//
+void	Config_T0(void)
+{
+	OPTION_REGbits.nWPUEN = 0;
+	OPTION_REGbits.TMR0CS = 0;			//Envia Inicio de Protocolo
+	OPTION_REGbits.TMR0SE = 0;			//0xAA:0x55
+	OPTION_REGbits.PSA = 0b0;
+	OPTION_REGbits.PS = 0b110;
+	//
+	TMR0 = 0;
+}
+//****************************************************************
+//	Funcion que Configura el Timer 1
+//	Entrada: Config_T1
+//****************************************************************
+//
+void	Config_T1_IR(void)
+{
+	T1CONbits.TMR1CS = 0b00;	// CLK = Fosc/4
+	T1CONbits.T1CKPS = 0b00;	// 
+	T1CONbits.nT1SYNC = 0b1;		//
+	T1CONbits.TMR1ON = 0b0;		//  
+}
+//****************************************************************
+//	Funcion que Configura el Timer 1
+//	Entrada: Config_T1
+//****************************************************************
+//
+void	Config_T1_mTouch(void)
+{
+	T1CONbits.TMR1CS = 0b11;	// CLK = Fosc/4
+	T1CONbits.T1CKPS = 0b00;	// 
+	T1CONbits.nT1SYNC = 0b1;		//
+	T1CONbits.TMR1ON = 0b1;		//  
+	//
+	T1GCONbits.TMR1GE = 1;
+	T1GCONbits.T1GPOL = 0;
+	T1GCONbits.T1GTM = 0;
+	T1GCONbits.T1GSPM = 0;
+	T1GCONbits.T1GVAL = 0;
+	T1GCONbits.T1GSS = 0b01;
+	//
+	TMR1 = 0;
+}
+//void putUART( char c){
+//  while ( PIR1bits.TXIF==0); //Espera a que se pueda Tx
+//  TXREG = c;
+//}
+//void EnvUART(char *Datos)
+//{
+//	while(*Datos != 0)
+//	{
+//		char c;
+////		c=*Datos
+//		putUART(*Datos);
+//		*Datos++;
+//	}
+//}
+//
+//****************************************************************
+//	Funcion que Configura EL Modulo mTouch
+//	Entrada: Config_CPS
+//****************************************************************
+//
+void	Config_CPS(void)
+{
+	CPSCON1 = 3;
+	//CPSCON0 = 0b10001101;
+	CPSCON0bits.T0XCS = 0b0;
+	CPSCON0bits.CPSRNG = 0b11;
+	CPSCON0bits.CPSRM = 0b0;
+	CPSCON0bits.CPSON = 0b1;
+}
+
+//****************************************************************
+//	Funcion que Configura las Interrupciones
+//	Entrada: Config_INT
+//****************************************************************
+//
+void	Config_INT(void)
+{
+//	OPTION_REGbits.nWPUEN = 0;
+	WPUAbits.WPUA2 = 1;
+	OPTION_REGbits.INTEDG = 0;
+	INTCONbits.INTF = 0;
+	INTCONbits.INTE = 1;
+	INTCONbits.TMR0IF = 0;
+	INTCONbits.TMR0IE = 1;
+	INTCONbits.PEIE = 1;
+	INTCONbits.GIE = 1;
+}
+//****************************************************************
+//	Funcion que Inicializa las Caliables utilizadas en el Programa
+//	Entrada: Initial_Var
+//****************************************************************
+//
+void Initial_Var(void)
+{
+	Cont = 0;
+	BanderasBits.Byte_Banderas = 0;
+//	HisteresisBits.Byte_Histeresis = 0;
+	Cont_Prog_IR = 0;
+}
+//****************************************************************
+//	Funcion que Compara si el dato Rx por IR es Igual al Patron
+//	Entrada: Comparar_IR
+//****************************************************************
+//
+char Comparar_IR(void)
+{
+	char i;
+	FlagIRBits.Igual_Patron = 1;
+	if(Patron_Cont == contador)
+	{
+		FlagIRBits.Igual_Patron = 1;
+		//Led_R = ~Led_R;
+	}
+	else
+		FlagIRBits.Igual_Patron = 0;
+	if(Patron_Start < (Time_start-250) || Patron_Start > (Time_start+250))
+		FlagIRBits.Igual_Patron = 0;
+	for(i=0; i<Patron_Cont; i++)
+	{
+		if(Patron_Bits[i] < (Time_Bits[i]-100) || Patron_Bits[i] > (Time_Bits[i]+100))
+			FlagIRBits.Igual_Patron = 0;
+	}
+	if(FlagIRBits.Igual_Patron == 1)
+	{
+		//Led_R = ~Led_R;
+		return(1);
+	}
+	else
+	{
+	//	Led_R = ~Led_R;
+		return(0);
+	}
+}
+//****************************************************************
+//	Funcion que Lee el Patron IR de la EEPROM
+//    Retorna 1 si hay Patron Almacebado y 0 en caso Contrario
+//	Entrada: Leer_Patron_IR
+//****************************************************************
+//
+char Leer_Patron_IR(void)
+{
+	char i;
+	char	Dato_L;		// 1 si Se Rx el Tiempo de START
+	char	Dato_H;		// 1 si Finalizo de Rx el dato IR
+	INTCONbits.GIE = 0;
+	Dato_L = eeprom_read (0);
+	if(Dato_L == 0xFF)
+	{
+		TMR0 = 0;
+		TMR1 = 0;
+		INTCONbits.TMR0IF = 0;
+		INTCONbits.GIE = 1;
+		return(0);
+	}
+	else
+	{
+		Patron_Cont = Dato_L;
+		Dato_L = eeprom_read (2);
+		Dato_H = eeprom_read (3);
+		Patron_Start = (Dato_H<<8 | Dato_L);
+		for(i=0;i<(Patron_Cont);i++)
+		{
+			Dato_L = eeprom_read ((i*2)+4);
+			Dato_H = eeprom_read (((i*2)+4)+1);
+			Patron_Bits[i] = (Dato_H<<8 | Dato_L);
+		}
+		TMR0 = 0;
+		TMR1 = 0;
+		INTCONbits.TMR0IF = 0;
+		INTCONbits.GIE = 1;
+		return(1);
+	}
+}
+
+//****************************************************************
+//	Funcion que Graba el Patron IR en la EEPROM
+//	Entrada: Graba_Patron_IR
+//****************************************************************
+//
+void Graba_Patron_IR(void)
+{
+	char i, Dato_L, Dato_H;
+	INTCONbits.GIE = 0;
+	Patron_Cont = contador;
+	Patron_Start = Time_start;
+	for(i=0; i<(contador); i++)
+		Patron_Bits[i] = Time_Bits[i];
+	eeprom_write(0, Patron_Cont);
+	Dato_L = Patron_Start;
+	Dato_H = Patron_Start>>8;
+	eeprom_write(2, Dato_L);
+	eeprom_write(3, Dato_H);
+	for(i=0;i<(Patron_Cont);i++)
+	{
+		Dato_L = Patron_Bits[i];
+		Dato_H = Patron_Bits[i]>>8;
+		eeprom_write ((i*2)+4,Dato_L);
+		eeprom_write (((i*2)+4)+1,Dato_H);
+	}
+	TMR0 = 0;
+	TMR1 = 0;
+	INTCONbits.TMR0IF = 0;
+	INTCONbits.GIE = 1;
+}
+
+//****************************************************************
+//	Funcion que Determina la Tecla IR presionada y la Guarda en
+//    la EEPROM siendo esta la tecla configurada
+//	Entrada: Programar_IR
+//****************************************************************
+//
+void Programar_IR(void)
+{
+	Rele = ~Rele;
+	FlagIRBits.Prog = 1;
+	Config_T1_IR();
+	T1CONbits.TMR1ON = 1;
+	INTCONbits.TMR0IF = 0;
+	//INTCONbits.TMR0IE = 0;
+	//BanderasBits.Inicio_IR = 0;
+	//BanderasBits.Ini_Start = 0;
+	//BanderasBits.Fin_IR = 0;
+	FlagIRBits.START = 0;
+	FlagIRBits.DATA = 0;
+	FlagIRBits.FinIR = 0;
+//	Cont = 0;
+	Time_start=0;
+	CPSCON0 = 0;
+	Cont_Prog_IR = 0;
+	TMR0 = 0;
+	//putUART("P");
+	while(FlagIRBits.FinIR == 0 && Cont_Prog_IR < Tiempo_Programar_IR)
+	{
+		if(INTCONbits.TMR0IF == 1)
+		{
+			//INTCONbits.TMR0IF = 0;
+			//Cont_Prog_IR++;
+			CLRWDT();
+		}
+	}
+	if(FlagIRBits.FinIR)
+	{
+		//putUART("p");
+		if(contador == 8 || contador == 16 || contador == 32)
+		{
+			Graba_Patron_IR();
+			Rele = ~Rele;
+			INTCONbits.TMR0IE = 0;
+		}
+	}
+	Cont_Prog_IR = 0;
+	FlagIRBits.START = 0;
+	FlagIRBits.DATA = 0;
+	FlagIRBits.FinIR = 0;
+	FlagIRBits.Prog = 0;
+	Cont=0;
+	//Config_T1_mTouch();
+	TMR0 = 0;
+	INTCONbits.TMR0IF = 0;
+	INTCONbits.TMR0IE = 1;
+	//CPSCON0bits.CPSON = 1;
+	Config_CPS();
+	Conteo_Ini = 0;
+	T2CONbits.TMR2ON = 0;
+	//TMR2 = 0;
+	//PIR1bits.TMR2IF = 0;
+	//PIE1bits.TMR2IE = 0;
+}
+//Hex2BCD(unsigned int HEXADECIMAL)
+//{
+//	char i;
+//	i=0;
+//	Num_BCD[0] = 0;
+//	Num_BCD[1] = 0;
+//	Num_BCD[2] = 0;
+//	Num_BCD[3] = 0;
+//	Num_BCD[4] = 0;
+//	while(HEXADECIMAL > 0)
+//	{
+//		Num_BCD[i] = HEXADECIMAL % 10;
+//		HEXADECIMAL /=10;
+//		i++; 
+//	}
+//}
+//char HEX2ASCII(char c)
+//{
+//	char ASCII;
+//	if(c < 0x0A)
+//		ASCII = c + 0x30;
+//	else
+//		ASCII = c + 0x37;
+//	return(ASCII);
+//}
+////****************************************************************
+////	Funcion que Configura el Modulo UART para que solo Tx a 19200
+////	Entrada: Ninguna
+////****************************************************************
+////
+//void Initial_UART(void)
+//{
+//	//Se Configura los Baudios
+//	SPBRG = baud;
+//	APFCONbits.TXCKSEL = 0;			//Selecciona RC6 como TX
+//	APFCONbits.RXDTSEL = 0;			//Selecciona RC7 como RX
+//	//Se Configura el Registro TXSTA
+//	TXSTAbits.CSRC = 0;				//Solo para Sincrono
+//	TXSTAbits.TX9  = 0;				//No Tx 9no Bit
+//	TXSTAbits.SYNC = 0;				//UART Asincrono
+//	TXSTAbits.BRGH = 1;				//Ecuacion Alta Vel
+//	TXSTAbits.TXEN = 1;				//Habilita Tx
+//	//TXSTA = 0x24;
+//	//Se Configura el Registro RCSTA
+//	RCSTAbits.RX9  = 0;				//Rx solo 8 bits
+//	RCSTAbits.SREN = 0;				//Rx solo para Sincrono
+//	RCSTAbits.CREN = 0;				//DesHabilita Rx
+//	RCSTAbits.RX9  = 0;				//Rx solo 8 bits
+//	RCSTAbits.ADDEN = 0;			//Deshabilita Dir Esclavo
+//	RCSTAbits.SPEN  = 1;			//Habilita Puerto Serial
+//	PIR1bits.RCIF = 0;
+//	//PIE1bits.RCIE = 0;
+//	Desh_INT_Rx;
+//	INTCONbits.PEIE = 1;
+//	INTCONbits.GIE = 1;
+//}
+//
+//void Env_Cod_IR(void)
+//{
+//	char i;
+//	//printf("Datos IR: \n");
+//	//printf("Cont IR: %d\n", Cont);
+//	//printf("Start IR: %d\n", Time_start);
+//	EnvUART("\n\rDatos IR: \n");
+//	EnvUART("Cont: ");
+//	Hex2BCD(contador);
+//	putUART(HEX2ASCII(Num_BCD[1]));
+//	putUART(HEX2ASCII(Num_BCD[0]));
+//	putUART(CR);
+//	putUART(LF);
+//	EnvUART("\n\rStart IR: ");
+//	Hex2BCD(Time_start);
+//	putUART(HEX2ASCII(Num_BCD[3]));
+//	putUART(HEX2ASCII(Num_BCD[2]));
+//	putUART(HEX2ASCII(Num_BCD[1]));
+//	putUART(HEX2ASCII(Num_BCD[0]));
+//	putUART(CR);
+//	putUART(LF);
+//	//printf("Contador %d\n", i,Time_Bits[i]);
+//	//printf("Cont: %2d",Cont);
+//	//printf("%d\n", Time_start);
+//	for(i=0; i<contador; i++)
+//	{
+//		EnvUART("Dato [");
+//		Hex2BCD(i);
+//		putUART(HEX2ASCII(Num_BCD[1]));
+//		putUART(HEX2ASCII(Num_BCD[0]));
+//		EnvUART("]: ");
+//		Hex2BCD(Time_Bits[i]);
+//		putUART(HEX2ASCII(Num_BCD[3]));
+//		putUART(HEX2ASCII(Num_BCD[2]));
+//		putUART(HEX2ASCII(Num_BCD[1]));
+//		putUART(HEX2ASCII(Num_BCD[0]));
+//		putUART(CR);
+//		putUART(LF);
+//		//printf("%d]: %d\n", i,Time_Bits[i]);
+//	}
+//		
+//	//}
+//}
+//
+//void	Tx_UART_Patron(void)
+//{
+//	char i;
+////	printf("Datos Patron IR: \n");	
+////	rintf("Cont Patron IR: %d\n", Patron_Cont);	
+////	printf("Start Patron IR: %d\n", Patron_Start);	
+////	for(i=0;i<Patron_Cont; i++)	
+////	{
+////		printf("Dato [%d]: %d\n", i,Patron_Bits[i]);
+////	}
+//	EnvUART("Cont Pat: ");
+//	Hex2BCD(Patron_Cont);
+//	putUART(HEX2ASCII(Num_BCD[1]));
+//	putUART(HEX2ASCII(Num_BCD[0]));
+//	putUART(CR);
+//	putUART(LF);
+//	EnvUART("\n\rStart P: ");
+//	Hex2BCD(Patron_Start);
+//	putUART(HEX2ASCII(Num_BCD[3]));
+//	putUART(HEX2ASCII(Num_BCD[2]));
+//	putUART(HEX2ASCII(Num_BCD[1]));
+//	putUART(HEX2ASCII(Num_BCD[0]));
+//	putUART(CR);
+//	putUART(LF);
+//	for(i=0; i<Patron_Cont; i++)
+//	{
+//		EnvUART("Dato [");
+//		Hex2BCD(i);
+//		putUART(HEX2ASCII(Num_BCD[1]));
+//		putUART(HEX2ASCII(Num_BCD[0]));
+//		EnvUART("]: ");
+//		Hex2BCD(Patron_Bits[i]);
+//		putUART(HEX2ASCII(Num_BCD[3]));
+//		putUART(HEX2ASCII(Num_BCD[2]));
+//		putUART(HEX2ASCII(Num_BCD[1]));
+//		putUART(HEX2ASCII(Num_BCD[0]));
+//		putUART(CR);
+//		putUART(LF);
+//	}
+//}
---
+++ PIC12_IR_Botton/Main_IR_Prueba.c
@@ -0,0 +1,414 @@
+//***************************************************/
+/*Programa para el que esta funcionando el el cuarto*/
+//***************************************************/
+#include <stdio.h>		
+#include <stdlib.h>
+#include <xc.h>
+#include <stdbool.h>
+
+
+#define Num_P   16
+#define Div		4
+#define Lib_Tec 10
+#define Inf_Tec 40
+
+#define LED_ON   0
+#define LED_OFF  1
+#define RELE_ON   1
+#define RELE_OFF  0
+
+//#define	Umbral_ST	3000	//Umbral Sin tapa
+//#define	Umbral_CT	350		//Umbral Con tapa
+//#define	Umbral_CT	600		//Umbral Con tapa
+//#define	Histereis	300		//Umbral Con tapa
+//#define	Umbral_CT	600		//Umbral Con tapa Vieja
+//#define	Histereis	250		//Histeresis Con tapa Vieja
+//#define	Histereis_Prom	100		//histeresis Con tapa nueva para hacer Promedio
+//
+//#define	Umbral_CT_N	900		//Umbral Con tapa nueva
+//#define	Histereis_N	450		//Histeresis Con tapa nueva
+//#define	Histereis_Prom_N	200		//histeresis Con tapa Vieja para hacer Promedio
+//
+//Con la tapa maaas nueva, con nueva figura posterior
+//#define	Umbral_CT_N	550		//Umbral Con tapa nueva
+//#define	Histereis_N	250		//Histeresis Con tapa nueva
+//#define	Histereis_Prom_N	100		//histeresis Con tapa Vieja para hacer Promedio
+//#define	Umbral_OFF	1150
+//#define	Umbral_ON	1250
+//#define	Hist_Prog	50
+
+#define	Max_Bits_IR	32
+#define	Tiempo_Programar_IR 125	//Se cuentan 125 Interrupciones de Timer 0
+								//A 32 ms genera 4 Seg de espera
+#define	CR	0x0D
+#define	LF	0x0A
+//#define Total_Bits  32
+#define Uno_Bit_IR 0x0001
+//Datos Leidos de la ultima tecla IR presionada
+unsigned int Tiempo_Bit;
+unsigned int Time_start;
+unsigned int Time_Bits[32];
+//Datos Patron IR a ser comparados. Patron
+unsigned int Patron_Start;
+unsigned int Patron_Bits[32];
+char		 Patron_Cont;
+char		Cont_Prog_IR;
+char		Cont_Prog_LED;
+char		Cont_Tec_Lib;
+//unsigned char Carac, Total_Bits_IR, Fin_T0, ind_prom,Conteo_Ini;
+unsigned char Cont, contador, Conteo_Ini;
+
+#include "Configuration_bit.c"
+
+#define TRIS_Rele	TRISAbits.TRISA1
+#define Rele			LATAbits.LATA1
+//Define los Leds que indicaran el estado o posibles fallas delmodulo
+//#define TRIS_Led_V		TRISAbits.TRISA0
+//#define Led_V			LATAbits.LATA0
+
+#define TRIS_Led_R		TRISAbits.TRISA5
+#define Led_R			LATAbits.LATA5
+
+#define TRIS_Led_Status		TRISAbits.TRISA4
+#define Led_Status			LATAbits.LATA4
+
+#define TRIS_Sw		TRISAbits.TRISA3
+#define Sw			PORTAbits.RA3
+char 			Num_BCD[6];
+#include "System.h"
+#include "User.h"
+#include "Funciones.c"
+
+int main(void) 
+{
+	char i;
+	OSCCON = 0b01101000;
+	if(PCONbits.nPOR == 0)
+	{
+		Rele = RELE_ON;
+		CLRWDT();
+		PCONbits.nPOR = 1;
+	}
+	TRIS_Rele = 0;
+	TRIS_Sw = 1;
+	OPTION_REGbits.nWPUEN = 0;
+//Leds de Pruebas
+//	Led_V = 1;
+	if(Rele)
+		Led_R = LED_ON;
+	else
+		Led_R = LED_OFF;
+//	TRIS_Led_V = 0;
+	TRIS_Led_R = 0;
+	TRIS_Led_Status = 0;
+	Led_Status = LED_OFF;
+
+//
+//	Umbral = Umbral_OFF;
+//	WDTCON = 0b00011011;	//Habilita WDT a 8 Seg 
+	ANSELA = 0;
+	APFCON = 0;
+	Conteo_Ini = 0;
+	Cont_Tec_Lib = 0;
+	Config_T0();
+	Config_INT();
+	Initial_Var();
+	Config_T1_IR();
+	Config_T2();
+	Initial_UART();
+	Cont_Prog_LED = 0;
+
+	Patron_Start = 0;
+	for(i=0; i<32; i++)
+		Patron_Bits[i] = 0;
+	if(Leer_Patron_IR() == 0)
+		Patron_Cont = 0;
+
+	BanderasBits.Tec_1V = 1;
+	INTCONbits.TMR0IE = 0;
+	PIE1bits.TMR2IE = 0;
+	putUART('P');
+	putUART('r');
+	putUART('u');
+	putUART('e');
+	putUART('b');
+	putUART('a');
+	putUART(CR);
+	putUART(LF);
+	Tx_UART_Patron();
+	EnvUART("Datos IR\n");
+	while(1)
+	{
+ 
+		if(!Sw)
+		{
+			__delay_ms(10);
+			//Cont_Prog_IR = 0;
+			if(!Sw)
+			{
+				if(BanderasBits.Tec_1V)
+				{
+					BanderasBits.Tec_1V = 0;
+					Cont_Prog_LED = 0;
+					Rele = ~Rele;
+					if(Rele)
+						Led_R = LED_ON;
+					else
+						Led_R = LED_OFF;
+					INTCONbits.TMR0IF = 0;
+					INTCONbits.TMR0IE = 1;
+				}
+				if(Cont_Prog_IR >= 50)
+				{
+					Led_R = ~Led_R;
+					Programar_IR();
+				}
+			}
+		}
+		else
+		{
+			Cont_Prog_IR = 0;
+			BanderasBits.Tec_1V = 1;
+			INTCONbits.TMR0IE = 0;
+		}
+		if(FlagIRBits.FinIR)
+		{
+			Led_Status = LED_OFF;
+			if(Patron_Cont != 0)
+			{
+				//Led_R = ~Led_R;
+				if(Comparar_IR()==1)
+					Rele = ~Rele;
+				if(Rele)
+					Led_R = LED_ON;
+				else
+					Led_R = LED_OFF;
+			}
+//			BanderasBits.Fin_IR = 0;
+			FlagIRBits.START = 0;
+			FlagIRBits.DATA = 0;
+			FlagIRBits.FinIR = 0;
+			Cont=0;
+			//Config_T1_mTouch();
+			TMR0 = 0;
+			INTCONbits.TMR0IF = 0;
+			INTCONbits.TMR0IE = 0;
+			//CPSCON0bits.CPSON = 1;
+			//Config_CPS();
+			Conteo_Ini = 0;
+			//T2CONbits.TMR2ON = 0;
+			//TMR2 = 0;
+			//PIR1bits.TMR2IF = 0;
+			//PIE1bits.TMR2IE = 0;
+//			Led_R = ~Led_R;
+			Env_Cod_IR();
+			putUART("E");
+			INTCONbits.INTE = 1;
+		}
+		CLRWDT();
+		
+	}
+}
+// Interrupcion de TMR0 e INT0
+// TMR0 interumpe cada 32 mSeg para el mTouch
+// INT0 interrumpe con el sensor IR en cada Flanco de Bajada
+void interrupt isr(void)
+{
+
+	if(INTCONbits.TMR0IF == 1 && INTCONbits.TMR0IE == 1)
+	{
+		INTCONbits.TMR0IF = 0;
+		//putUART('t');
+		Cont_Prog_IR++;
+		if(FlagIRBits.Prog)
+		{
+			Cont_Prog_LED++;
+			if(Cont_Prog_LED >= 10)
+			{
+				Cont_Prog_LED = 0;
+				Led_R = ~Led_R;
+			}
+		}
+	}	
+   if(INTCONbits.INTE == 1 && INTCONbits.INTF == 1)
+	{
+		//putUART('I');
+		Tiempo_Bit = TMR1;
+		TMR1 = 0;
+		TMR2 = 0;
+		INTCONbits.INTF = 0;
+		if(FlagIRBits.DATA)						//Se esta Rx los Bits IR?
+		{
+			Time_Bits[Cont++]=Tiempo_Bit;
+			TMR2 = 0;
+			putUART('d');
+			//putUART('F');
+			if(Tiempo_Bit > (Time_start+500))
+			{
+				FlagIRBits.FinIR = 1;
+				contador = Cont-1;
+				Config_T2_FinIR();
+				T2CONbits.TMR2ON = 1;
+				INTCONbits.INTE = 0;
+				FlagIRBits.Sleep_IR = 1;
+				Led_Status = LED_OFF;
+				putUART('f');
+			}
+			else if(Cont >= Max_Bits_IR)
+			{
+				FlagIRBits.FinIR = 1;
+				contador = Cont;
+				Config_T2_FinIR();
+				T2CONbits.TMR2ON = 1;
+				INTCONbits.INTE = 0;
+				FlagIRBits.Sleep_IR = 1;
+				Led_Status = LED_OFF;
+				putUART('a');
+			}
+			else if(!FlagIRBits.Prog)
+			{
+				if(Cont == Patron_Cont)
+				{
+					FlagIRBits.FinIR = 1;
+					contador = Cont;
+					Config_T2_FinIR();
+					T2CONbits.TMR2ON = 1;
+					INTCONbits.INTE = 0;
+					FlagIRBits.Sleep_IR = 1;
+					Led_Status = LED_OFF;
+					putUART('F');
+				}
+			}
+			//putUART('d');
+		}
+		else if(FlagIRBits.START == 1)
+		{
+				FlagIRBits.START = 0;
+				FlagIRBits.DATA = 1;
+				FlagIRBits.FinIR = 0;
+				Cont = 0;
+				Time_start = Tiempo_Bit;
+				TMR2 = 0;
+//				if(FlagIRBits.Prog == 0)
+//				{
+//					if(Patron_Cont)
+//					{
+//						if(Patron_Start < (Time_start-550) || Patron_Start > (Time_start+550))
+//						{
+//							FlagIRBits.START = 0;
+//							FlagIRBits.DATA = 0;
+//							FlagIRBits.FinIR = 0;
+//							Cont=0;
+//							Config_T1_mTouch();
+//							TMR0 = 0;
+//							INTCONbits.TMR0IF = 0;
+//							INTCONbits.TMR0IE = 1;
+//							//CPSCON0bits.CPSON = 1;
+//							Config_CPS();
+//							Conteo_Ini = 0;
+//							TMR2 = 0;
+//							PIR1bits.TMR2IF = 0;
+//							//PIE1bits.TMR2IE = 0;
+//							Config_T2_FinIR();
+//							T2CONbits.TMR2ON = 1;
+//							INTCONbits.INTE = 0;
+//							FlagIRBits.Sleep_IR = 1;
+//						}
+//					}
+//				}
+				putUART('S');
+		}
+		else
+		{
+			//Config_T1_IR();
+			T1CONbits.TMR1ON = 1;
+			INTCONbits.TMR0IF = 0;
+			INTCONbits.TMR0IE = 0;
+			FlagIRBits.DATA = 0;
+			FlagIRBits.START = 1;
+			FlagIRBits.Sleep_IR = 0;
+			Cont = 0;
+			Time_start=0;
+			CPSCON0 = 0;
+			TMR2 = 0;
+			//Config_T2();
+			//T2CONbits.TMR2ON = 1;
+			TMR2 = 0;
+			PIR1bits.TMR2IF = 0;
+			PIE1bits.TMR2IE = 1;
+			T2CONbits.TMR2ON = 1;
+			//Estaus LED = ON
+			Led_Status = LED_ON;
+			putUART('i');
+		}
+	}
+	if(PIR1bits.TMR2IF == 1 && PIE1bits.TMR2IE == 1)
+	{
+		putUART('T');
+		if(FlagIRBits.Sleep_IR == 1)
+		{
+			//Led_R = ~Led_R;
+			T2CONbits.TMR2ON = 0;
+			TMR2 = 0;
+			PIR1bits.TMR2IF = 0;
+			PIE1bits.TMR2IE = 0;
+			INTCONbits.INTF = 0;
+			INTCONbits.INTE = 1;
+			FlagIRBits.Sleep_IR = 0;
+		}
+		else
+		{
+			if(Cont >= Max_Bits_IR)
+			{
+				FlagIRBits.FinIR = 1;
+				contador = Cont;
+				Config_T2_FinIR();
+				T2CONbits.TMR2ON = 1;
+				INTCONbits.INTE = 0;
+				FlagIRBits.Sleep_IR = 1;
+				Led_Status = LED_OFF;
+			}
+			else if(FlagIRBits.Prog == 1)
+			{
+				FlagIRBits.FinIR = 1;
+				contador = Cont;
+				Config_T2_FinIR();
+				T2CONbits.TMR2ON = 1;
+				INTCONbits.INTE = 0;
+				FlagIRBits.Sleep_IR = 1;
+				Led_Status = LED_OFF;
+			}
+			else if(Cont == Patron_Cont)
+			{
+				FlagIRBits.FinIR = 1;
+				contador = Cont;
+				Config_T2_FinIR();
+				T2CONbits.TMR2ON = 1;
+				INTCONbits.INTE = 0;
+				FlagIRBits.Sleep_IR = 1;
+				Led_Status = LED_OFF;
+			}
+			else
+			{
+				//Config_T1_mTouch();
+				//Config_CPS();
+				//TMR0 = 0;
+				//INTCONbits.TMR0IF = 0;
+				//INTCONbits.TMR0IE = 1;
+				//CPSCON0bits.CPSON = 1;
+				FlagIRBits.START = 0;
+				FlagIRBits.DATA = 0;
+				FlagIRBits.FinIR = 0;
+				FlagIRBits.Sleep_IR = 1;
+				Cont = 0;
+				Conteo_Ini = 0;
+				TMR2 = 0;
+				T2CONbits.TMR2ON = 0;
+				PIR1bits.TMR2IF = 0;
+				PIE1bits.TMR2IE = 0;
+				Led_Status = LED_OFF;
+			}
+
+		}
+	}
+}
