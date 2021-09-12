505c75434d661a77a44ece182dd1ca74faadec5a
Antonio Pateti <50499248+apateti@users.noreply.github.com>
2021-09-12 08:05:31 -0400

    Create Teclas_CR.h

---
+++ PIC12675_IR/Teclas_CR.h
@@ -0,0 +1,43 @@
+#nolist
+
+#define Tecla_01	0xF001
+#define Tecla_02	0xE002
+#define Tecla_03	0xD003
+#define Tecla_04	0xC004
+#define Tecla_05	0xB005
+#define Tecla_06	0xA006
+#define Tecla_07	0x9007
+#define Tecla_08	0x8008
+#define Tecla_09	0x7009
+#define Tecla_00	0x0000
+
+#define Tecla_LAST	0xC013
+#define Tecla_SELECT	0xE011
+#define Tecla_MENU	0x6019
+#define Tecla_MUTE	0x100F
+#define Tecla_CHAN_Mas	0x500B
+#define Tecla_CHAN_Men	0x400C
+#define Tecla_VOL_Mas	0x300D
+#define Tecla_VOL_Men	0x200E
+#define Tecla_PAGE_Mas	0x303A
+#define Tecla_PAGE_Men	0x203B
+#define Tecla_GUIDE	0xD030
+#define Tecla_INFO	0xA033
+#define Tecla_EXIT	0xD012
+#define Tecla_FF	0x201D					//tecla >>
+#define Tecla_RR	0x101E					//tecla <<
+#define Tecla_PLAY	0x5038
+#define Tecla_PAUSE	0x001F
+#define Tecla_O		0xC031
+#define Tecla_STOP	0x4039
+#define Tecla_O_II	0x8017
+#define Tecla_O_ID	0x7027
+#define Tecla_O_DI	0x6028
+#define Tecla_O_DD	0xB032
+
+#define Tecla_Der	0x6037
+#define Tecla_Izq	0x7036
+#define Tecla_Ar	0x9034
+#define Tecla_Ab	0x8035
+
+#list
